bf3d0f54f6790af69178c10128f82cf2f77b8c38
Thiago Ximenes <thiagoximeneslima@gmail.com>
2022-03-30 11:14:57 -0300

    create test and implementation of getCategories

--- src/services/fetchData.ts
+++ src/services/fetchData.ts
@@ -1,11 +1,20 @@
 import axios from 'axios';
 
-// eslint-disable-next-line import/prefer-default-export
 export async function getByQuery(query : string): Promise<object> {
   try {
     const response = await axios.get(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
 
     return response.data.results;
+  } catch (err) {
+    return { err };
+  }
+}
+
+export async function getCategories() {
+  try {
+    const response = await axios.get('https://api.mercadolibre.com/sites/MLB/categories');
+
+    return response.data;
   } catch (err) {
     return { err };
   }
