29fcd80578402ac2401226480091251c89eea445
Thiago Ximenes <thiagoximeneslima@gmail.com>
2022-03-30 10:12:33 -0300

    refactory getByQuery function to return object always

--- src/services/fetchData.ts
+++ src/services/fetchData.ts
@@ -1,10 +1,12 @@
 import axios from 'axios';
 
-export async function getByQuery(query) {
+// eslint-disable-next-line import/prefer-default-export
+export async function getByQuery(query : string): Promise<object> {
   try {
     const response = await axios.get(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
+
     return response.data.results;
-  } catch(err) {
-    console.log(err);
+  } catch (err) {
+    return { err };
   }
 }
